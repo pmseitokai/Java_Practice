8889ab7e6790c903ec9e17c999fa74028847e4ab
pmseitokai <nitit.punch@mail.kmutt.ac.th>
2017-04-20 04:03:24 +0700

    switch case for operator

--- operator.java
+++ operator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package myprogram;
+package myapp.Java_Practice.Java_Practice;
 
 import java.util.Scanner;
 
